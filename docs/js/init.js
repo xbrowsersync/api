7803e340589aabcc99a654f094d4d12b1bb6458d
nero120 <rich.dmz@protonmail.com>
2016-09-09 09:28:08 +0200

    Updated build script. Fixes to index.html.

--- docs/js/init.js
+++ docs/js/init.js
@@ -5,12 +5,6 @@ xBrowserSync.API.Site = xBrowserSync.API.Site || {};
 xBrowserSync.API.Site.Init = function() {
     'use strict';
     
-    var serviceStatuses = {
-        online: 1,
-        offline: 2,
-        noNewSyncs: 3
-    };
-
     $('.scroll').bind('click', function(t) {
         var l = $(this);
 
@@ -33,24 +27,30 @@ xBrowserSync.API.Site.Init = function() {
     });
 
     $(function() {
-        $.get('/info', function(response) {
-            $('#version').text(response.version);
-            
-            switch (response.status) {
-                case 1:
-                    $('#currentstatus').text('Online').addClass('success');
-                    break;
-                case 3:
-                    $('#currentstatus').text('Not accepting new syncs').addClass('warning');
-                    break;
-                default:
-                case 2:
-                    $('#currentstatus').text('Offline').addClass('danger');
-                    break;
-            }
+        console.log('test');
+        $.ajax({
+            method: 'GET',
+            url: '/info',
+            timeout: 5000
         })
-        .fail(function() {
-            $('#status').text('Offline').addClass('danger');
-        });
+            .done(function(response) {
+                $('#version').text(response.version);
+                
+                switch (response.status) {
+                    case 1:
+                        $('#currentstatus').text('Online').addClass('success');
+                        break;
+                    case 3:
+                        $('#currentstatus').text('Not accepting new syncs').addClass('warning');
+                        break;
+                    default:
+                    case 2:
+                        $('#currentstatus').text('Offline').addClass('danger');
+                        break;
+                }
+            })
+            .fail(function() {
+                $('#currentstatus').text('Offline').addClass('danger');
+            });
     });
 }();
