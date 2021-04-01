c149f1fd4c00027b995dad576568ce894ae6d9c6
nero120 <rich.dmz@protonmail.com>
2021-04-01 17:59:02 +0100

    Fixed docs build issue. Removed redudant deps.

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   devServer: {
     contentBase: Path.join(__dirname, 'dist'),
     compress: true,
-    port: 8082,
+    port: 8083,
   },
   module: {
     rules: [
@@ -35,11 +35,7 @@ module.exports = {
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
       {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: 'file-loader',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        test: /\.(svg|jpg|gif|woff|woff2|eot|ttf|otf)$/,
         use: 'file-loader',
       },
     ],
