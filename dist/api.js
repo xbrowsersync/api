4ff767a43c37478c5409e84c98d6a72fd0194c63
Stefan Warnat <ich@stefanwarnat.de>
2023-11-22 18:53:31 +0100

    remove dist folder

--- dist/api.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./server");
-// Entry point into server
-exports.default = (async () => {
-    const app = await server_1.createApplication();
-    await server_1.startService(app);
-})();
