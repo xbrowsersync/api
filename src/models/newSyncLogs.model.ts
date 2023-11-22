aa1c54af83910908ccb144a7337a768f448d48cb
Stefan Warnat <ich@stefanwarnat.de>
2023-11-22 17:33:55 +0100

    TypeORM models

--- src/models/newSyncLogs.model.ts
+++ src/models/newSyncLogs.model.ts
@@ -1,34 +1,29 @@
 import moment from 'moment';
-import mongoose from 'mongoose';
-import * as uuid from 'uuid';
+import { BaseEntity, Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-// Interface for new sync log model
 export interface INewSyncLog {
   expiresAt?: Date;
   ipAddress?: string;
   syncCreated?: Date;
 }
 
-export interface INewSyncLogsModel extends INewSyncLog, mongoose.Document {}
+@Entity()
+export class NewSyncLog extends BaseEntity implements INewSyncLog {
+  @PrimaryGeneratedColumn()
+  _id: string;
 
-// Create new sync logs schema to store ip address and created date
-// No concurrent updates so disable version keys
-const newSyncLogsSchema = new mongoose.Schema(
-  {
-    _id: { type: String, default: uuid.v4 },
-    expiresAt: {
-      default: () => moment().add(1, 'days').startOf('day').toDate(),
-      type: Date,
-    },
-    ipAddress: String,
-    syncCreated: {
-      default: () => new Date(),
-      type: Date,
-    },
-  },
-  {
-    versionKey: false,
-  }
-);
+  @Column({
+    type: Date,
+    default: moment().add(1, 'days').startOf('day').format('YYYY-MM-DD hh:mm:ss'),
+  })
+  expiresAt: Date;
 
-export const NewSyncLogsModel = mongoose.model<INewSyncLogsModel>('NewSyncLog', newSyncLogsSchema, 'newsynclogs');
+  @Column({
+    type: String,
+    length: 64,
+  })
+  ipAddress: string;
+
+  @CreateDateColumn()
+  syncCreated: Date;
+}
